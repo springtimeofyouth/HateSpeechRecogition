62f071bfeee95cb8db87928ae155efca012713ec
Md Toufik <md.toufik@iitg.ac.in>
2024-01-13 20:09:43 +0530

    Added folder structure and notebook

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Md Toufik",
+    author_email="md.toufik@iitg.ac.in",
+    packages=find_packages(),
+    install_requires=[],
+)
